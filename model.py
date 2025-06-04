322f4a82017e8a429bcee5c9abbebe707f8c3046
seungil <s24060@gsm.hs.kr>
2025-06-04 20:53:00 +0900

    update: 모델 변경 ResNet50 -> MobileNet_v2

--- model.py
+++ model.py
@@ -2,25 +2,10 @@ from scipy.cluster.hierarchy import weighted
 from torch import nn
 from torchvision import models
 
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.baseResNet = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-        self.baseResNet.fc = nn.Sequential(
-            nn.Linear(self.baseResNet.fc.in_features, 128),
-            nn.ReLU(),
-            nn.Linear(128, 7)
-        )
-
-    def forward(self, x):
-        x = self.baseResNet(x)
-        return x
-
-
 class EmotionMobileNet(nn.Module):
     def __init__(self, num_classes=7):
         super(EmotionMobileNet, self).__init__()
-        self.base_model = models.mobilenet_v2(weights=None)
+        self.base_model = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.DEFAULT)
         self.base_model.classifier = nn.Sequential(
             nn.Dropout(0.3),
             nn.Linear(self.base_model.last_channel, 128),
